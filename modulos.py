b686cea3a84b05d6bcf63926563e7503a6251f1d
Jonas Lima de Araújo <81834101+JonasLimaDev@users.noreply.github.com>
2022-06-01 16:11:20 -0300

    Update modulos.py
    Mudança nas dependências.
    Alterada forma de realização do Download.
    adicionado a opção para baixar vários arquivos em sequência.
    adicionado algumas mensagens de erros

--- modulos.py
+++ modulos.py
@@ -1,39 +1,55 @@
 from bs4 import BeautifulSoup
-import urllib3
 import requests
-from ftplib import FTP
+import os
+import wget
+
+
+def verificar_entrada(entrada):
+	lista_entrdas = []
+	if ',' in entrada:
+		lista_entrdas = entrada.split(',')
+	else:
+		lista_entrdas=[entrada]
+	print(lista_entrdas)
+	return(lista_entrdas)
+
 
 def buscar_link(id):
-	print("buscando link\n")
-	#http = urllib3.PoolManager()
-	#html=http.request('GET',"https://www.ncbi.nlm.nih.gov/genome/"+str(id))
+	print("buscando link")
+	dados = {}
 	html= requests.get("https://www.ncbi.nlm.nih.gov/genome/"+str(id))
-	bs = BeautifulSoup(html.text,'html.parser')
-	resultados= bs.find_all('span')
-	for r in resultados:
-		
-		if "Download sequences in FASTA format for genome" in r.get_text() :
-			texto=r.find('a')
-			link=texto['href']
-	print("Link encontrado")
-	return link
+	if html.status_code == 200:
+		bs = BeautifulSoup(html.text,'html.parser')
+		resultados= bs.find_all('span')		
+		for r in resultados:
+			if "Download sequences in FASTA format for genome" in r.get_text() :
+				texto=r.find('a')
+				dados['link'] = texto['href']
+		resultados_2  = bs.find_all('span',{"class": "GenomeTitle"})
+		organismo = resultados_2[0].get_text()
+		organismo = organismo.split("(")[0][:-1]
+		dados['nome'] = organismo
+		print(f"link para {organismo} \033[92mEncontrado\033[0m")
+		return dados
+	elif html.status_code == 404:
+		print(f"Nada Encontrado para o ID {id}")
+		return None
+	else:
+		print(f"Erro Inesperado para o ID {id}")
+		return None
 
 
-def baixar(link):
-	arquivo=link.split('/')[-1]
-	#print(arquivo)
-	site=link.rsplit(arquivo, 1)[0]
-	site=site.rsplit("ftp://ftp.ncbi.nlm.nih.gov/", 1)[1]
-	#print(site)
-	ftp = FTP('ftp.ncbi.nlm.nih.gov')
-	ftp.login()
-	ftp.cwd(site)
-	files = ftp.nlst()
-	#print(files)
-	for file in files:
-		if file == arquivo:
-			print("Baixando... " + file)
-			ftp.retrbinary("RETR " + file ,open("./" + file, 'wb').write)
-			print("\nFinalizado transferência do arquivo: " + file)
-	ftp.close()
+def baixar(dados):
+	if dados != None:
+		local_download =  f"./downloads/{dados['nome']}"
+		if not os.path.exists(local_download):
+			os.makedirs(local_download)
+		print(f"Baixando: {dados['nome']}")
+		wget.download(dados['link'],local_download)
+		print(f"\nFinalizado o download do Genoma: {dados['nome']}")
+	else:
+		pass
+
+if __name__ == "__main__":
+	pass
 
