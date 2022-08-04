d5bf4e5ee5aed78678130a4096f0301f0675a2f8
Jonas Lima de Araújo <81834101+JonasLimaDev@users.noreply.github.com>
2022-08-04 10:36:38 -0300

    opções  baixar ou buscar pasta

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from modulos import buscar_link, baixar, verificar_entrada
+from modulos import buscar_link, baixar, verificar_entrada,get_link_dados
 
 if __name__ == "__main__":
     id_downloads = verificar_entrada(input("Digite o código do genoma: "))
     lista_downloads = []
+    print("Opções:\n1 - Download do Genoma\n2 - Obter Link da Pasta")
+    opcao = input("Digite a opção: ")
+
     for id in id_downloads:
         info=buscar_link(id)
         if info != None:
             lista_downloads.append(info)
     for dados in lista_downloads:
-        baixar(dados)
+        if opcao == "1":
+            baixar(dados)
+        elif opcao == "2":
+            get_link_dados(dados)
+        else:
+            print("Opção inválida tente novamente")
+
